7f4319ab3888d25b516b61d75b01308380606b8e
Affan Haq <haqgamer66@gmail.com>
2020-10-26 01:13:03 -0400

    removed videos controller

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,9 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UsersController;
-use App\Http\Controllers\VideosController;
 use App\Http\Livewire\Videos\CreateVideo;
 use App\Http\Livewire\Videos\ShowVideo;
+use App\Models\Video;
 
 Auth::routes();
 
@@ -15,7 +15,17 @@ Route::prefix('user')->group(function () {
 });
 
 Route::prefix('videos')->group(function () {
-    Route::get('/', [VideosController::class, 'index'])->name('videos.index');
-    Route::get('create', CreateVideo::class)->name('videos.create');
-    Route::get('{slug}', ShowVideo::class)->name('videos.show');
+    Route::get('/', function () {
+        $videos = Video::orderBy('created_at', 'desc')
+            ->where('converted_for_streaming_at', '<>', null)
+            ->get();
+        return view('video.index')->with('videos', $videos);
+    })->name('videos.index');
+
+    Route::get('create', CreateVideo::class)
+        ->middleware('auth')
+        ->name('videos.create');
+
+    Route::get('{slug}', ShowVideo::class)
+        ->name('videos.show');
 });
