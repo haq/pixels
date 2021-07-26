4dfd11168d781be1c0052a9fa7d7ae3ef03d2339
haq <affan.haq@outlook.com>
2021-07-26 01:33:43 -0400

    disabled video scaling

--- app/Jobs/ConvertVideoForStreaming.php
+++ app/Jobs/ConvertVideoForStreaming.php
@@ -21,22 +21,22 @@ class ConvertVideoForStreaming implements ShouldQueue
 
     public function handle()
     {
-        $lowBitrate = (new X264($audioCodec = 'libmp3lame'))->setKiloBitrate(1500);
-        $midBitrate = (new X264($audioCodec = 'libmp3lame'))->setKiloBitrate(3500);
-        $highBitrate = (new X264($audioCodec = 'libmp3lame'))->setKiloBitrate(7000);
+        $lowBitrate = (new X264)->setKiloBitrate(1500);
+        $midBitrate = (new X264)->setKiloBitrate(3500);
+        $highBitrate = (new X264)->setKiloBitrate(7000);
 
         FFMpeg::fromDisk($this->video->disk)
             ->open($this->video->path)
             ->exportForHLS()
             ->toDisk('minio')
             ->addFormat($lowBitrate, function ($media) {
-                $media->scale(960, 720);
+                //$media->scale(960, 720);
             })
             ->addFormat($midBitrate, function ($media) {
-                $media->scale(1280, 720);
+                //$media->scale(1280, 720);
             })
             ->addFormat($highBitrate, function ($media) {
-                $media->scale(1920, 1080);
+                //$media->scale(1920, 1080);
             })
             ->save('videos/' . $this->video->uuid . '/video.m3u8');
 
